13be21847c48b95ba2eebdbf707b663c82ce3c40
a-wilr <146993826+a-wilr@users.noreply.github.com>
2023-11-27 22:03:56 -0500

    first steps
    they grow up so fast

--- Assets/Player/PlayerController.cs
+++ Assets/Player/PlayerController.cs
@@ -10,6 +10,8 @@ public class PlayerController : MonoBehaviour
     private float Speed = 7;
     private float verticalSpeed = 7;
     private bool IsGrounded;
+    public Animator animator; 
+    public SpriteRenderer spriteRenderer; 
     
     [SerializeField] private Rigidbody2D rb;
     [SerializeField] private Transform tf;
@@ -29,6 +31,11 @@ public class PlayerController : MonoBehaviour
     private void Move()
     {
         rb.velocity = new Vector2(HorizontalInput * Speed, rb.velocity.y);
+
+        checkXDirection();
+
+        //begin walk animation
+        animator.SetFloat("speed", MathF.Abs(rb.velocity.x));
     }
 
     private void Jump()
@@ -36,11 +43,18 @@ public class PlayerController : MonoBehaviour
         if (Input.GetButtonDown("Jump") && isGrounded())
         {
             rb.velocity = new Vector2(rb.velocity.x, verticalSpeed);
+            checkXDirection();
+            animator.SetBool("isJump", true);
+            animator.SetBool("isGrounded", false);
         }
-        
-        if (Input.GetButtonUp("Jump") && rb.velocity.y > 0f)
+        else if (Input.GetButtonUp("Jump") && rb.velocity.y > 0f)
         {
             rb.velocity = new Vector2(rb.velocity.x, rb.velocity.y * 0.5f);
+        }
+        else
+        {
+            animator.SetBool("isJump", false);
+            animator.SetBool("isGrounded", true);
         }
         
     }
@@ -50,5 +64,18 @@ public class PlayerController : MonoBehaviour
         return Physics2D.OverlapCircle(tf.position, 0.2f, groundLayer);
     }
     
-    
+    private void checkXDirection()
+    {
+         //flip direction boomerman is facing based on input
+        if(rb.velocity.x > 0) 
+        {
+            spriteRenderer.flipX = true;
+        }
+        else if(rb.velocity.x < 0) 
+        {
+            spriteRenderer.flipX = false;
+        }
+    }
+
+    //to do: death(), nake sure animator.isDead set to true
 }
